3292ffdbecf0d5759644b9a7dae582588a616531
Rahulydv-15 <126334481+Rahulydv-15@users.noreply.github.com>
2023-07-10 09:24:35 +0530

    Update DeviceInfo.js

--- models/DeviceInfo.js
+++ models/DeviceInfo.js
@@ -21,8 +21,7 @@ const DeviceInfoSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-  ,{ timestamps: true }
-});
+},{ timestamps: true });
 
 export default mongoose.model('DeviceInfo', DeviceInfoSchema);
 
